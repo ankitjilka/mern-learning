99724a5517f91ba39a37cec5dc11b73d8215d094
Ankit Jilka <ankitjilka10@gmail.com>
2025-06-14 16:39:33 +0530

    code updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
 
 const app = express();
 const PORT = process.env.PORT
 
-connectDB();
-
 // middleware
 app.use(express.json())
-app.use((req, res, next) => {
-    console.log("we just get a new request");
-    next();
-})
+app.use(rateLimiter)
+//explanation of middleware
+// app.use((req, res, next) => {
+//     console.log(`Request method is ${req.method} && REQUEST url is ${req.url}`);
+//     next();
+// })
 
 app.use("/api/notes", notesRoutes);
 
+connectDB().then(() => {
 app.listen(PORT, () => {
     console.log("server started on port:", PORT);
+  });
 });
-
